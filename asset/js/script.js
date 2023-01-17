1152b3083d676922192490bb2f3718a3f1e3e29d
Md. Atikur Rahman <58600348+atikrpi27@users.noreply.github.com>
2023-01-18 00:45:20 +0600

    Add font, update structure and navbar rotate icon

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,49 +1,42 @@
 //========================= JavaScript =========================//
 
-let dropdownBtn = document.querySelectorAll('.dropdown-toggle1');
+// let dropdownBtn = document.querySelectorAll('.dropdown-toggle1');
 
 // For navbar rotate icon
 
-dropdownBtn.forEach(drop => {
-    drop.addEventListener('click', function(){
-        let dropdownIcon = drop.querySelector('.navbar-arrow')
+// dropdownBtn.forEach(drop => {
+//     drop.addEventListener('click', function(){
+//         let dropdownIcon = drop.querySelector('.navbar-arrow')
 
-        if (drop.classList.contains('show')){
-            dropdownIcon.classList.add('arrow')
-        }
-        else{
-            dropdownIcon.classList.remove('arrow')
-        }
-    })
-})
+//         if (drop.classList.contains('show')){
+//             dropdownIcon.classList.add('arrow')
+//         }
+//         else{
+//             dropdownIcon.classList.remove('arrow')
+//         }
+//     })	
+// })
 
 //========================= JQuery =========================//
 
 // For navbar rotate icon
 
-// $(document).ready(function () {
-//     $(document).on("click", ".dropdown-toggle1", function () {
-//         let parent = $(this).closest(".dropdown");
-//         let child = parent.find(".navbar-arrow");
-//         child.addClass("arrow");
+$(document).ready(function(){
+    $(document).on('click', '.dropdown-toggle1', function(){
+        let parent=$(this).closest('.dropdown')
+        let child =parent.find('.navbar-arrow')
+        child.addClass('arrow')
 
-//         let show = parent.find(".show").attr("class");
-//         // console.log("show",show)
-//         if ($(this).hasClass("show")) {
-//             console.log("show");
-//             child.addClass("arrow");
-//         }
+        if($(this).hasClass("show")){
+            console.log("show")
+            child.addClass('arrow')
+        }
+        else{
+            child.removeClass('arrow')
+        }
+    })
 
-//         // else if($(this).not('show')){
-//         //     console.log("remove")
-//         //     child.removeClass('arrow')
-//         // }
-//         else {
-//             child.removeClass("arrow");
-//         }
-//     });
-
-//     $("body").on("click", function () {
-//         $(".navbar-arrow").removeClass("arrow");
-//     });
-// });
+    $('body').on("click", function () {
+        $(".navbar-arrow").removeClass("arrow");
+      })
+})
