0b96ad8ee92174562427f04d5647b3347786e9ba
Md. Atikur Rahman <58600348+atikrpi27@users.noreply.github.com>
2023-01-17 21:46:21 +0600

    update and apply js for navbar icon rotate

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,54 +1,49 @@
-//================== JavaScript ==================//
+//========================= JavaScript =========================//
 
-// const dropdownMenu = document.querySelector(".dropdown");
-// // const dropdownBtn = document.querySelector(".dropdown-toggle1");
-// const toggleArrow = document.querySelector(".navbar-arrow");
+let dropdownBtn = document.querySelectorAll('.dropdown-toggle1');
 
-// // Toggle dropdown function
-// const toggleDropdown = function () {
-//     dropdownMenu.classList.toggle("show");
-//     toggleArrow.classList.toggle("arrow");
-// };
+// For navbar rotate icon
 
-// // Toggle dropdown open/close when dropdown button is clicked
-// // dropdownMenu.forEach( drop => drop.addEventListener("click",function(e){
-// //     e.stopPropagation();
-// //     toggleDropdown();
-// // }))
+dropdownBtn.forEach(drop => {
+    drop.addEventListener('click', function(){
+        let dropdownIcon = drop.querySelector('.navbar-arrow')
 
-// dropdownMenu.addEventListener("click", function (e) {
-//     e.stopPropagation();
-//     toggleDropdown();
+        if (drop.classList.contains('show')){
+            dropdownIcon.classList.add('arrow')
+        }
+        else{
+            dropdownIcon.classList.remove('arrow')
+        }
+    })
+})
+
+//========================= JQuery =========================//
+
+// For navbar rotate icon
+
+// $(document).ready(function () {
+//     $(document).on("click", ".dropdown-toggle1", function () {
+//         let parent = $(this).closest(".dropdown");
+//         let child = parent.find(".navbar-arrow");
+//         child.addClass("arrow");
+
+//         let show = parent.find(".show").attr("class");
+//         // console.log("show",show)
+//         if ($(this).hasClass("show")) {
+//             console.log("show");
+//             child.addClass("arrow");
+//         }
+
+//         // else if($(this).not('show')){
+//         //     console.log("remove")
+//         //     child.removeClass('arrow')
+//         // }
+//         else {
+//             child.removeClass("arrow");
+//         }
+//     });
+
+//     $("body").on("click", function () {
+//         $(".navbar-arrow").removeClass("arrow");
+//     });
 // });
-
-// // Close dropdown when dom element is clicked
-// document.addEventListener("click", function () {
-//     if (dropdownMenu.classList.contains("show")) {
-//         toggleDropdown();
-//     }
-// });
-
-//================== Jquery ==================//
-
-$(document).ready(function () {
-  $(document).on("click", ".dropdown-toggle1", function () {
-    let parent = $(this).closest(".dropdown");
-    let child = parent.find(".navbar-arrow");
-    child.addClass("arrow");
-
-    let show = parent.find(".show").attr("class");
-    console.log("show", show);
-
-    if ($(this).hasClass("show")) {
-      // alert("show")
-      console.log("show");
-      child.addClass("arrow");
-    } else {
-      child.removeClass("arrow");
-    }
-  });
-
-  $("body").on("click", function () {
-    $(".navbar-arrow").removeClass("arrow");
-  });
-});
